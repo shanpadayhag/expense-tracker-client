e24b2fc24fe874bb3fd8612c39188c36d4c3ebe0
Shan Padayhag <shanpadayhag@gmail.com>
2023-06-18 23:26:02 +0800

    Unnecessary class

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -10,33 +10,31 @@ import 'package:expense_tracker_client/features/funds/domain/usecases/get_funds.
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
-class DependencyClass extends GetxController {
-  void init() {
-    // Controllers
-    Get.lazyPut<DashboardController>(
-        () => DashboardController(enumUtil: Get.find<EnumUtil>()));
-    Get.lazyPut<SegmentControlController>(() => SegmentControlController());
+void init() {
+  // Controllers
+  Get.lazyPut<DashboardController>(
+      () => DashboardController(enumUtil: Get.find<EnumUtil>()));
+  Get.lazyPut<SegmentControlController>(() => SegmentControlController());
 
-    // Use cases
-    Get.lazyPut<AddFund>(() => AddFund(Get.find<FundRepositoryImpl>()));
-    Get.lazyPut<GetFunds>(() => GetFunds(Get.find<FundRepositoryImpl>()));
+  // Use cases
+  Get.lazyPut<AddFund>(() => AddFund(Get.find<FundRepositoryImpl>()));
+  Get.lazyPut<GetFunds>(() => GetFunds(Get.find<FundRepositoryImpl>()));
 
-    // Repositories
-    Get.lazyPut<FundRepositoryImpl>(() =>
-        FundRepositoryImpl(sqliteSource: Get.find<FundSQLiteSourceImpl>()));
+  // Repositories
+  Get.lazyPut<FundRepositoryImpl>(
+      () => FundRepositoryImpl(sqliteSource: Get.find<FundSQLiteSourceImpl>()));
 
-    // Datasources
-    Get.lazyPut<FundSQLiteSourceImpl>(
-        () => FundSQLiteSourceImpl(database: Get.find<DriftDB>()));
+  // Datasources
+  Get.lazyPut<FundSQLiteSourceImpl>(
+      () => FundSQLiteSourceImpl(database: Get.find<DriftDB>()));
 
-    // Configs
-    Get.lazyPut<DriftDB>(() => DriftDB());
+  // Configs
+  Get.lazyPut<DriftDB>(() => DriftDB());
 
-    // Utils
-    Get.lazyPut<EnumUtil>(() => EnumUtil());
-    Get.lazyPut<CurrencyUtil>(() => CurrencyUtil());
+  // Utils
+  Get.lazyPut<EnumUtil>(() => EnumUtil());
+  Get.lazyPut<CurrencyUtil>(() => CurrencyUtil());
 
-    // External
-    Get.lazyPut<http.Client>(() => http.Client());
-  }
+  // External
+  Get.lazyPut<http.Client>(() => http.Client());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:expense_tracker_client/core/routes/routes.dart';
 import 'package:expense_tracker_client/core/themes/light_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'injection_container.dart';
+import 'injection_container.dart' as di;
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  DependencyClass().init();
+  di.init();
   runApp(GetMaterialApp(
     theme: lightTheme,
     initialRoute: Routes.initialRoute,
